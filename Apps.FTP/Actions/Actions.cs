c0db4cd45caed0db40196f4be4e1290ee91d35cc
Mihaly Hobor <mihaly.hobor@blackbird.io>
2025-03-18 16:33:19 +0100

    Updated FTP functionalities in Actions.cs and added DirectoryItemDto.

--- Apps.FTP/Actions/Actions.cs
+++ Apps.FTP/Actions/Actions.cs
@@ -1,3 +1,5 @@
+using Apps.FTP.Api;
+using Apps.FTP.Dtos;
 using Apps.FTP.Models.Requests;
 using Apps.FTP.Models.Responses;
 using Blackbird.Applications.Sdk.Common;
@@ -5,8 +7,6 @@ using Blackbird.Applications.Sdk.Common.Actions;
 using Blackbird.Applications.Sdk.Common.Files;
 using Blackbird.Applications.Sdk.Common.Invocation;
 using Blackbird.Applications.SDK.Extensions.FileManagement.Interfaces;
-using System.Net.Mime;
-using System.Threading.Tasks;
 
 namespace Apps.FTP.Actions;
 
@@ -23,7 +23,7 @@ public class Actions : FTPInvocable
     [Action("Upload File", Description = "Uploads a file to the FTP server")]
     public async Task UploadFile(UploadFileRequest uploadFileRequest)
     {
-       
+        await Client.Connect();
         using (var file = await _fileManagementClient.DownloadAsync(uploadFileRequest.File))
         {
 
@@ -34,6 +34,8 @@ public class Actions : FTPInvocable
     [Action("Download File", Description = "Downloads a file from the FTP server")]
     public async Task<DownloadFileResponse> DownloadFile(DownloadFileRequest downloadFileRequest)
     {
+        await Client.Connect();
+
         using (var stream = new MemoryStream())
         {
             await Client.DownloadStream(stream,downloadFileRequest.Path);
@@ -53,30 +55,49 @@ public class Actions : FTPInvocable
     [Action("List Directory", Description = "Lists the contents of a directory on the FTP server")]
     public async Task<ListDirectoryResponse> ListDirectory(ListDirectoryRequest listDirectoryRequest)
     {
-        throw new NotImplementedException();
+        await Client.Connect();
+        var listings = await Client.GetListing(listDirectoryRequest.Path, FluentFTP.FtpListOption.Recursive);
+
+        var items = listings.Select(i => new DirectoryItemDto()
+        {
+            Name = i.Name,
+            Path = i.FullName
+        });
+
+        return new ListDirectoryResponse()
+        {
+            DirectoriesItems = items
+        };
     }
 
     [Action("Delete File", Description = "Deletes a file from the FTP server")]
     public async Task DeleteFile(string remoteFilePath)
     {
+        await Client.Connect();
+
         await Client.DeleteFile(remoteFilePath);
     }
 
     [Action("Rename file", Description = "Rename a path from old to new")]
     public async Task RenameFile([ActionParameter] RenameFileRequest input)
     {
+        await Client.Connect();
+
         await Client.Rename(input.OldPath, input.NewPath);
     }
 
     [Action("Create directory", Description = "Create new directory by path")]
     public async Task CreateDirectory([ActionParameter] CreateDirectoryRequest input)
     {
+        await Client.Connect();
         await Client.CreateDirectory(input.Path);
     }
 
     [Action("Delete directory", Description = "Delete directory by path")]
     public async Task DeleteDirectory([ActionParameter] DeleteDirectoryRequest input)
     {
+        await Client.Connect();
+
         await Client.DeleteDirectory(input.Path);
     }
 }
